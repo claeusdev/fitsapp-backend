f352e8d6cf3c093f31ba36303554787d05734338
Nana Adjei Manu <claeusdev@gmail.com>
2017-09-27 04:50:23 +0000

    Bumped up UX

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/show'
+
   resources :messages do
   	collection do
   		get :inbox
@@ -7,6 +9,7 @@ Rails.application.routes.draw do
   end
   devise_for :trainers
   devise_for :users
+  resources :users, :only => [:show]
   root 'pages#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
