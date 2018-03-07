6ac69881c3d61966d883dd65be2a31ca49987237
Nana Adjei Manu <claeusdev@gmail.com>
2018-03-07 04:54:21 +0000

    Add validation to messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,8 @@
 class Message < ApplicationRecord
 	belongs_to :sender, class_name: "User"
 	belongs_to :recipient, class_name: "User"
+
+	validates :sender, presence: true
+	validates :recipient, presence: true
+	
 end
